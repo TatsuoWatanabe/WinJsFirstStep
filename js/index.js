e50428cd74583b2335953ce1fb896ad45587d630
渡部龍朗 <tatsuo1234567@gmail.com>
2014-10-01 23:54:27 +0900

    commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+﻿$(function () {
+    WinJS.UI.processAll().done(function () {
+        // WinJS.UI.NavBar
+        var $navBar = $('#createNavBar');
+        var navBar = $navBar[0].winControl;
+
+        $navBar.on('beforehide beforeshow', function (ev) {
+            var marginTop = (navBar.hidden) ? '50px' : '100px';
+            $('#pageHost').animate({ marginTop: marginTop }, 100);
+        });
+
+        navBar.addEventListener('invoked', function (ev) {
+            var navbarCommand = ev.detail.navbarCommand;
+            console.log(navbarCommand.label);
+        });
+
+        navBar.show();
+    });
+});
+//# sourceMappingURL=index.js.map
