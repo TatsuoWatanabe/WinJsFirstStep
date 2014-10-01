37d52c964b7544d491fcaef09eb1a8bfda7eb18a
渡部龍朗 <tatsuo1234567@gmail.com>
2014-10-01 23:57:30 +0900

    commit

---
+++ pages/pages/pages.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    WinJS.UI.Pages.define('pages/pages/pages.html', {
+        ready: function (element, options) {
+            // Render the page control via a call to WinJS.UI.Pages.render. This lets
+            // you render a page control by referencing it via url.
+            var renderHost = $('.renderingPageControls-renderedControl')[0];
+            WinJS.UI.Pages.render("pages/pages/samplePageControl.html", renderHost).done(function () {
+                var bindData = WinJS.Binding.as({
+                    controlText: 'This control created by calling WinJS.UI.Pages.render'
+                });
+
+                // Data bind to the child tree to set the control text
+                WinJS.Binding.processAll(renderHost, bindData);
+
+                $(renderHost).find('.samplePageControl-button').on('click', function () {
+                    bindData.controlText = 'button clicked.';
+                });
+
+                // Event handler for when the toggle control switches
+                $(renderHost).find('.samplePageControl-toggle').on('change', function () {
+                    var toggleControl = this.winControl;
+                    bindData.controlText = 'toggle is now ' + toggleControl.checked;
+                });
+            });
+
+            return this;
+        }
+    });
+})();
+//# sourceMappingURL=pages.js.map
